d121940eb1e83c14aab1eb5b41bd851c1ea82dd0
HORBAY02 <HorbachZhenja@yandex.by>
2023-06-07 11:11:17 +0200

    initialization project

---
+++ App.tsx
@@ -0,0 +1,129 @@
+import React from 'react';
+import {
+  Button,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+import {Header} from 'react-native/Libraries/NewAppScreen';
+
+import Braze from '@braze/react-native-sdk';
+
+function Section({
+  cardDescription,
+  clicked,
+  created,
+  dismissed,
+  dismissible,
+  domain,
+  id,
+  pinned,
+  title,
+  type,
+  url,
+}: Braze.ContentCard): JSX.Element {
+  return (
+    <View style={styles.sectionContainer}>
+      <Text style={styles.point}>
+        <Text style={styles.pointName}>Title:</Text>
+        {title}
+      </Text>
+      <Text style={styles.point}>
+        <Text style={styles.pointName}>CardDescription:</Text>
+        {cardDescription}
+      </Text>
+      <Text style={styles.point}>
+        <Text style={styles.pointName}>ID:</Text> {id}
+      </Text>
+      <Text style={styles.point}>
+        <Text style={styles.pointName}>Domain:</Text>
+        {domain}
+      </Text>
+      <Text style={styles.point}>
+        <Text style={styles.pointName}>Clicked:</Text>
+        {`${clicked}`}
+      </Text>
+      <Text style={styles.point}>
+        <Text style={styles.pointName}>Created:</Text> {`${created}`}
+      </Text>
+      <Text style={styles.point}>
+        <Text style={styles.pointName}>Dismissed:</Text> {`${dismissed}`}
+      </Text>
+      <Text style={styles.point}>
+        <Text style={styles.pointName}>Dismissible:</Text> {`${dismissible}`}
+      </Text>
+      <Text style={styles.point}>
+        <Text style={styles.pointName}>Pinned:</Text> {`${pinned}`}
+      </Text>
+      <Text style={styles.point}>
+        <Text style={styles.pointName}>Type:</Text> {type}
+      </Text>
+      <Text style={styles.point}>
+        <Text style={styles.pointName}>URL:</Text> {url}
+      </Text>
+    </View>
+  );
+}
+
+function App(): JSX.Element {
+  const [cards, setCards] = React.useState<Braze.ContentCard[]>([]);
+
+  Braze.addListener(Braze.Events.CONTENT_CARDS_UPDATED, async update => {
+    setCards(update.cards);
+  });
+
+  console.log(cards);
+
+  React.useEffect(() => {
+    Braze.changeUser('208900362');
+  }, []);
+
+  const openContentCards = () => {
+    Braze.launchContentCards();
+    Braze.requestContentCardsRefresh();
+  };
+
+  Braze.subscribeToInAppMessage(true, () => {});
+
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <Header />
+      <ScrollView style={{flex: 1}}>
+        {cards.map(card => {
+          return <Section {...card} key={card.expiresAt} />;
+        })}
+        <Button onPress={openContentCards} title="OPEN CONTENT CARDS" />
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginHorizontal: 10,
+    padding: 5,
+    borderRadius: 10,
+    backgroundColor: 'white',
+    borderColor: 'black',
+    borderWidth: 2,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  pointName: {fontWeight: '600', fontSize: 12},
+  point: {fontSize: 10},
+});
+
+export default App;
---
+++ ios/AwesomeProject/AppDelegate.mm
@@ -0,0 +1,101 @@
+#import "AppDelegate.h"
+#import <BrazeKit/BrazeKit-Swift.h>
+#import "BrazeReactBridge.h"
+#import <React/RCTRootView.h>
+#import <React/RCTLinkingManager.h>
+
+#import <React/RCTBundleURLProvider.h>
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+  self.moduleName = @"AwesomeProject";
+  // You can add your custom initial props in the dictionary below.
+  // They will be passed down to the ViewController used by React Native.
+  self.initialProps = @{};
+
+    // Setup Braze bridge
+  NSURL *jsCodeLocation =
+      [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
+  RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
+                                                      moduleName:@"AwesomeProject"
+                                               initialProperties:nil
+                                                   launchOptions:launchOptions];
+
+  // Configure views in the application
+  self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+  UIViewController *rootViewController = [UIViewController new];
+  rootViewController.view = rootView;
+  self.window.rootViewController = rootViewController;
+  [self.window makeKeyAndVisible];
+
+  // Setup Braze
+  BRZConfiguration *configuration = [[BRZConfiguration alloc] initWithApiKey:@""
+                                                                    endpoint:@"sdk.fra-01.braze.eu"];
+  // - Enable logging and customize the configuration here
+  configuration.logger.level = BRZLoggerLevelInfo;
+  Braze *braze = [BrazeReactBridge initBraze:configuration];
+  AppDelegate.braze = braze;
+
+  /* Other configuration */
+
+  return YES;
+}
+
+#pragma mark - AppDelegate.braze
+
+static Braze *_braze = nil;
+
++ (Braze *)braze {
+  return _braze;
+}
+
++ (void)setBraze:(Braze *)braze {
+  _braze = braze;
+}
+
+
+- (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
+{
+#if DEBUG
+  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
+#else
+  return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+#endif
+}
+
+/// This method controls whether the `concurrentRoot`feature of React18 is turned on or off.
+///
+/// @see: https://reactjs.org/blog/2022/03/29/react-v18.html
+/// @note: This requires to be rendering on Fabric (i.e. on the New Architecture).
+/// @return: `true` if the `concurrentRoot` feature is enabled. Otherwise, it returns `false`.
+- (BOOL)concurrentRootEnabled
+{
+  return true;
+}
+
+- (BOOL)application:(UIApplication *)application
+   openURL:(NSURL *)url
+   options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options
+{
+  return [RCTLinkingManager application:application openURL:url options:options];
+}
+
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url
+  sourceApplication:(NSString *)sourceApplication annotation:(id)annotation
+{
+  return [RCTLinkingManager application:application openURL:url
+                      sourceApplication:sourceApplication annotation:annotation];
+}
+
+- (BOOL)application:(UIApplication *)application continueUserActivity:(nonnull NSUserActivity *)userActivity
+ restorationHandler:(nonnull void (^)(NSArray<id<UIUserActivityRestoring>> * _Nullable))restorationHandler
+{
+ return [RCTLinkingManager application:application
+                  continueUserActivity:userActivity
+                    restorationHandler:restorationHandler];
+}
+
+@end
+
